e78163d5e35ea60cecb308ff5f48c3c349b4e802
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-09-05 11:25:51 -0400

    I changed the names

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: 9/4/2024
+ * This program type Emre Guzel
 */
 
-basic.showString('Hello, World!')
+basic.showString('Emre Guzel')
